51222f44463b9db685ead67bc6e761f351a0f197
Rudraprosad <166218847+Rudraprosad@users.noreply.github.com>
2026-07-16 18:04:53 +0530

    payment section removed

--- src/components/About/Project/Project.jsx
+++ src/components/About/Project/Project.jsx
@@ -1,7 +1,7 @@
 import "./Project.scss";
 
 import Button from "../Button/Button";
-import PaymentSection from "./PaymentSection";
+//import PaymentSection from "./PaymentSection";
 
 const projectData = {
   one: {
@@ -101,11 +101,11 @@ const Project = ({ projectID }) => {
         </div>
       ))}
 
-      <PaymentSection
-        projectName={project.name}
-        price={project.price}
-        projectId={projectID}
-      />
+      {/* <PaymentSection */}
+        {/* projectName={project.name} */}
+        {/* price={project.price} */}
+        {/* projectId={projectID} */}
+      {/* /> */}
     </div>
   );
 };
