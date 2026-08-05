1b4909648f0dcf4f58df8202de61c51e74c50d29
Rudraprosad <166218847+Rudraprosad@users.noreply.github.com>
2026-08-05 18:50:17 +0530

    ooooo

--- src/Experience/models/DetailT.jsx
+++ src/Experience/models/DetailT.jsx
@@ -67,7 +67,7 @@ export default function Model({ progress = 0, pulseIntensity = 0, ...props }) {
   return (
     <group {...props} dispose={null}>
       <mesh
-        geometry={nodes.obj12.geometry}
+        geometry={nodes.detail_Baked.geometry}
         material={materials["MergedBake_Baked.010"]}
         position={[-6.46, 69.669, -1.148]}
         rotation={[Math.PI / 2, 0, 0]}
